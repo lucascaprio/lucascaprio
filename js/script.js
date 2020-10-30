e7defbf977ccc230a67469c600ed6bcc7b5753b0
Lucas Caprio <lucasfernandescaprio@gmail.com>
2020-10-30 16:28:47 -0300

    update google analytics code

--- js/script.js
+++ js/script.js
@@ -6,20 +6,31 @@
 	});
 
 	// Google Analytics snippet code
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-58044444-1', 'auto');
-	ga('send', 'pageview');
+	// ga('create', 'UA-58044444-1', 'auto');
+	// ga('send', 'pageview');
+	
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-D9VDXGWGRC');
+
 
 	// social links events to Google Analytics
 	var socialLinks = document.querySelectorAll('.social-links');
 
 	socialLinks.forEach(function(link) {
 		link.addEventListener('click', function() {
-			ga('send', 'event', 'Social Links', 'click', link.innerHTML);
+			// ga('send', 'event', 'Social Links', 'click', link.innerHTML);
+			gtag('event', 'click', {
+				'event_category': 'social-links',
+				'event_label': link.innerHTML.toLocaleLowerCase()
+			});
 		});
 	});
 }(window, document));
